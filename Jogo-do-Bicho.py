0521bd924581b780c4ab9f55bda7e11cee2a736f
Cristiano Santos <87429136+7e15@users.noreply.github.com>
2022-02-10 09:19:24 -0300

    Create Jogo-do-Bicho.py
    This is an quick code, part of an work on progress of mine, in the coming up updates I will add defs, tuples and if/elses, on top of that an randomizer code to generate the weekly-results.

---
+++ Jogo-do-Bicho.py
@@ -0,0 +1,29 @@
+ani_Avestruz = ["Avestruz",1,2,3,4]     ## Jogo do Bicho by: 7e15 vol.1
+ani_Aguia = [5,6,7,8]
+ani_Burro = [9,10,11,12]
+ani_Borboleta = [13,14,15,16]
+ani_Cachorro = [17,18,19,20]
+ani_Cabra = [21,22,23,24]
+ani_Carneiro = [25,26,27,28]
+ani_Camelo = [29,30,31,32]
+ani_Cobra = [33,34,35,36]
+ani_Coelho = [37,38,39,40]
+ani_Cavalo = [41,42,43,44]
+ani_Elefante = ["Elefante",45,46,47,48]
+ani_Galo = ["Galo",49,50,51,52]
+ani_Gato = [53,54,55,56]
+ani_Jacare = [57,58,59,60]
+ani_Leao = [61,62,63,64]
+ani_Macaco = [65,66,67,68]
+ani_Porco = ["Porco",69,70,71,72]
+ani_Pavao = [73,74,75,76]
+ani_Peru = [77,78,79,80]
+ani_Touro = [81,82,83,84]
+ani_Tigre = [85,86,87,88]
+ani_Urso = [89,90,91,92]
+ani_Veado = ["Veado", 93,94,95,96]
+ani_Vaca = [97,98,99,100]
+
+print(f"Você apostou em: ", ani_Galo, ani_Elefante)
+print(f"Os bichos sorteados desta semana foram: ", ani_Porco, ani_Veado)
+print("Oh não, você perdeu!! HAHAHAHAHHA")
